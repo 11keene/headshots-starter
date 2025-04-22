0fd29ffbd14281d62203edb4028927f4139252e2
11keene <support@aimavenstudio.com>
2025-04-22 19:26:26 -0400

    Fix OR import, move component and update login layout

---
+++ app/login/components/OR.tsx
@@ -0,0 +1,11 @@
+// components/OR.tsx
+export default function OR() {
+    return (
+      <div className="flex items-center my-1">
+        <div className="border-b flex-grow mr-2 opacity-50" />
+        <span className="text-sm opacity-50">OR</span>
+        <div className="border-b flex-grow ml-2 opacity-50" />
+      </div>
+    );
+  }
+  
