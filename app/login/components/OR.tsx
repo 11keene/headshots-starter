35ed81e678217e7cb94ac5a27810261ca744f822
11keene <support@aimavenstudio.com>
2025-04-23 09:08:14 -0400

    Add login page route + client Login component + OR divider

--- app/login/components/OR.tsx
+++ app/login/components/OR.tsx
@@ -1,10 +1,11 @@
-// components/OR.tsx
-export default function OR() {
+// app/components/OR.tsx
+
+export function OR() {
     return (
-      <div className="flex items-center my-1">
-        <div className="border-b flex-grow mr-2 opacity-50" />
-        <span className="text-sm opacity-50">OR</span>
-        <div className="border-b flex-grow ml-2 opacity-50" />
+      <div className="flex items-center my-4 w-full max-w-md">
+        <div className="border-b flex-grow opacity-50" />
+        <span className="px-2 text-sm opacity-50">OR</span>
+        <div className="border-b flex-grow opacity-50" />
       </div>
     );
   }
