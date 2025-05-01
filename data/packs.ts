864eba609eeb354c0e5f2c607b6b8a0131873d7a
11keene <support@aimavenstudio.com>
2025-05-01 18:40:38 -0400

    add photos

--- data/packs.ts
+++ data/packs.ts
@@ -8,10 +8,10 @@ export interface Pack {
   
   export const packs: Pack[] = [
     { id: "j-crew",  name: "J. Crew",                         exampleImg: "/images/bob.png" },
-    { id: "bold-colors", name: "Bold Colors",                exampleImg: "/images/bold-colors.jpg" },
+    { id: "bold-colors", name: "Bold Colors",                exampleImg: "/images/bold-colors.png" },
     { id: "thumbnail",   name: "YouTube Thumbnail Reaction", exampleImg: "/images/pixiecut.png" },
     { id: "glamour",     name: "Glamour Shot",               exampleImg: "/images/straight.png" },
-    { id: "vikings",     name: "Vikings",                    exampleImg: "/images/pasthoulderlength.png" },
+    { id: "vikings",     name: "Vikings",                    exampleImg: "/images/pastshoulderlength.png" },
     { id: "dating",      name: "Dating",                     exampleImg: "/images/straight.png" },
   ];
   
