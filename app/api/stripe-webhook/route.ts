5b30ffecfbb3fd10b0377a742a5e1296bf2c8b1e
11keene <aaronkeene11@gmail.com>
2025-07-23 12:39:37 -0400

    fix code

--- app/api/stripe-webhook/route.ts
+++ app/api/stripe-webhook/route.ts
@@ -25,7 +25,7 @@ export async function POST(req: Request) {
   }
 
   // —————————————————————————————————————————————
-  // STEP B: Verify that this really came from Stripe
+  // STEP B: Verify that this really came from Stripes
   // —————————————————————————————————————————————
   let event: Stripe.Event;
   try {
